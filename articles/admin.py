7c29a027ec75ab81f0f6b214eb2a0b7636616efd
Alexander <alokdev@mail.ru>
2021-12-20 00:48:17 +0300

    add BaseInlineFormSet

--- articles/admin.py
+++ articles/admin.py
@@ -6,20 +6,28 @@ from .models import Article, ArticleTags, Tag
 
 class RelationshipInlineFormset(BaseInlineFormSet):
     def clean(self):
+        self.curent_tag = False
+
         for form in self.forms:
             # В form.cleaned_data будет словарь с данными
-            # каждой отдельной формы, которые вы можете проверить
             form.cleaned_data
-            print(form.cleaned_data)
 
-            # if form.cleaned_data['is_main'] is False:
-            #     raise ValidationError('Укажите основной раздел')
+            if self.curent_tag and form.cleaned_data.get('is_main'):
+                raise ValidationError('Главный может быть только 1')
+            else:
+                if form.cleaned_data.get('is_main'):
+                    print(f"{form.cleaned_data.get('tag')} - главный раздел")
+                    self.curent_tag = True
+                else:
+                    continue
+
+        if not form.cleaned_data and not self.curent_tag:
+            raise ValidationError('Не выбран главный раздел')
 
             # вызовом исключения ValidationError можно указать админке о наличие ошибки
             # таким образом объект не будет сохранен,
             # а пользователю выведется соответствующее сообщение об ошибке
 
-            # raise ValidationError('Тут всегда ошибка')
         return super().clean()  # вызываем базовый код переопределяемого метода
 
 class RelationshipInline(admin.TabularInline):

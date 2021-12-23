6ff418504c3488d2a846b732a7a201f825df42e7
Alexander <alokdev@mail.ru>
2021-12-23 23:41:26 +0300

    BaseInlineFormSet correct

--- articles/admin.py
+++ articles/admin.py
@@ -4,41 +4,38 @@ from django.core.exceptions import ValidationError
 from django.forms import BaseInlineFormSet
 from .models import Article, ArticleTags, Tag
 
-class RelationshipInlineFormset(BaseInlineFormSet):
-    def clean(self):
-        self.curent_tag = False
-
-        for form in self.forms:
-            # В form.cleaned_data будет словарь с данными
-            form.cleaned_data
-
-            if self.curent_tag and form.cleaned_data.get('is_main'):
-                raise ValidationError('Главный может быть только 1')
-            else:
-                if form.cleaned_data.get('is_main'):
-                    print(f"{form.cleaned_data.get('tag')} - главный раздел")
-                    self.curent_tag = True
-                else:
-                    continue
-
-        if not form.cleaned_data and not self.curent_tag:
-            raise ValidationError('Не выбран главный раздел')
-
-            # вызовом исключения ValidationError можно указать админке о наличие ошибки
-            # таким образом объект не будет сохранен,
-            # а пользователю выведется соответствующее сообщение об ошибке
-
-        return super().clean()  # вызываем базовый код переопределяемого метода
-
-class RelationshipInline(admin.TabularInline):
-    model = ArticleTags
-    formset = RelationshipInlineFormset
-
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
     list_display = ['id', 'name']
 
+class ArticleTagsInlineFormset(BaseInlineFormSet):
+    def clean(self):
+
+        if len(self.forms) == 0:
+            raise ValidationError('Не указаны теги!')
+
+        self.count_is_main_tag = 0
+
+        for form in self.forms:
+            if self.count_is_main_tag > 0 and form.cleaned_data.get('is_main'):
+                raise ValidationError('Главный может быть только 1 тег')
+            else:
+                if form.cleaned_data.get('is_main'):
+                    print(f"{form.cleaned_data.get('tag')} - главный раздел")
+                    self.count_is_main_tag += 1
+                else:
+                    continue
+
+        return super().clean()
+
+
+class ArticleTagsInline(admin.TabularInline):
+    model = ArticleTags
+    formset = ArticleTagsInlineFormset
+    extra = 0
+
+
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ['id', 'title']
-    inlines = [RelationshipInline]
+    inlines = [ArticleTagsInline]
